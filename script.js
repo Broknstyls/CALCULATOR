cddf03d800ed59bf3fe9e97e091c48f0ddff7492
Broknstyls <muban90@gmail.com>
2023-11-01 22:17:39 +0200

    added percent?

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 let answer = document.getElementById('display').textContent;
 let c = document.getElementById('c').onclick=
 function() {
-    document.getElementById('display').textContent = 0}
+    document.getElementById('display').textContent = 'Hello There!'}
     let percent = document.getElementById('percent').onclick=
     function(){
-        if(answer == 0 || answer == undefined || answer=='')
-        document.getElementById('display').textContent = '%';
-    } else (
-        document.getElementById('display').textContent = `${answer} %`;)
-    
+        if(document.getElementById('display').textContent == 'Hello There!'){
+            document.getElementById('display').textContent = '%'
+        }
+        else if(
+            document.getElementById('display').textContent == '%'
+        ){
+            let repeat = '%'
+            document.getElementById('display').textContent = repeat + '%'
+        } else{
+        document.getElementById('display').textContent =  answer + '%';}
+    } 

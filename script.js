1074b3712afdaa5b5f25e11fa22cf2833c0e744c
Broknstyls <muban90@gmail.com>
2023-11-01 22:11:06 +0200

    added percent?

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ let answer = document.getElementById('display').textContent;
 let c = document.getElementById('c').onclick=
 function() {
     document.getElementById('display').textContent = 0}
+    let percent = document.getElementById('percent').onclick=
+    function(){
+        if(answer == 0 || answer == undefined || answer=='')
+        document.getElementById('display').textContent = '%';
+    } else (
+        document.getElementById('display').textContent = `${answer} %`;)
+    

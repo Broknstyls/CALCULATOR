590a68e55045401a8ff907ecf24bd235042bca71
Broknstyls <muban90@gmail.com>
2023-11-02 22:26:54 +0200

    adding divisionstuff

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-let answer = document.getElementById('display').textContent;
+let answer = document.getElementById('display').innerHTML;
 let c = document.getElementById('c').onclick=
 function() {
     document.getElementById('display').textContent = 'Hello There!'}
-    let percent = document.getElementById('percent').onclick=
+
+
+ let percent = document.getElementById('percent').onclick=
     function(){
         if(document.getElementById('display').textContent == 'Hello There!'){
             document.getElementById('display').textContent = '%'
@@ -15,3 +17,16 @@ function() {
         } else{
         document.getElementById('display').textContent =  answer + '%';}
     } 
+let divide = document.getElementById('divide').onclick=
+    function(){
+        if(document.getElementById('display').textContent == 'Hello There!'){
+            document.getElementById('display').textContent = '/';
+        } 
+        else if(document.getElementById('display').textContent == '/'){
+            let repeat = '/'
+            document.getElementById('display').textContent = repeat + '/'
+        }
+        else{
+            document.getElementById('display').textContent = answer + '/'
+        }
+    }

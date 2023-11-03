0c4457524f6eff8a9581d587da6b4b30d86921d8
Broknstyls <muban90@gmail.com>
2023-11-03 11:05:08 +0200

    added 7, working

--- script.js
+++ script.js
@@ -30,3 +30,12 @@ let divide = document.getElementById('divide').onclick=
             document.getElementById('display').textContent = answer + '/'
         }
     }
+    let seven = document.getElementById('7').onclick=
+        function(){
+            if(document.getElementById('display').textContent == 'Hello There!'){
+                document.getElementById('display').textContent = '7'
+            } else if(document.getElementById('display').textContent == 0 ||document.getElementById('display').textContent == '0' )
+            {document.getElementById('display').textContent = 7 } else{
+                let answer = document.getElementById('display').textContent
+                document.getElementById('display').textContent = answer + 7 }
+        }
